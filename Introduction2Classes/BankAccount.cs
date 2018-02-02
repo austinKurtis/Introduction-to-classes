51552baa740e69967809cdf0889f2ae5b54f0862
AustinKurtis <austin.jay.kurtis@gmail.com>
2018-02-02 15:53:41 -0600

    working on the deposits and withdrawal methods.

--- Introduction2Classes/BankAccount.cs
+++ Introduction2Classes/BankAccount.cs
@@ -1,29 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Introduction2Classes
 {
     public class BankAccount
     {
         public string Number { get;  }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance { 
+            get 
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        
+        }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
-            
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
 
         public void MakeWithdrawl(decimal amount, DateTime date, string note)
         {
-            
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive")
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
         }
         public BankAccount(string name, decimal initialBalance)
         {
             this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
             this.Balance = initialBalance;
+
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
         }
 
         private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransactions = new List<Transaction>() >;
     }
 }
